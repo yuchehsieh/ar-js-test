f2964f824c7720df4571a1bb54ec1036f3500376
Allen Chen (Invoker) <e2ne00@gmail.com>
2022-06-04 16:49:47 +0800

    feat: change the way

--- src/index.js
+++ src/index.js
@@ -8,16 +8,24 @@ import { StoreProvider } from './store';
 import AppRoutes from './routes';
 import AppProvider from './layouts/App';
 
+import { ARCanvas, ARMarker } from '@artcom/react-three-arjs';
+
 ReactDOM.render(
-    <React.StrictMode>
-        <StoreProvider>
-            <BrowserRouter>
-                <AppProvider>
-                    <AppRoutes />
-                </AppProvider>
-            </BrowserRouter>
-        </StoreProvider>
-    </React.StrictMode>,
+    <ARCanvas
+        camera={{ position: [0, 0, 0] }}
+        onCreated={({ gl }) => {
+            gl.setSize(window.innerWidth, window.innerHeight);
+        }}
+    >
+        <ambientLight />
+        <pointLight position={[10, 10, 0]} />
+        <ARMarker type={'pattern'} patternUrl={'data/hiro.patt'}>
+            <mesh>
+                <boxBufferGeometry args={[1, 1, 1]} />
+                <meshStandardMaterial color={'green'} />
+            </mesh>
+        </ARMarker>
+    </ARCanvas>,
     document.getElementById('root'),
 );
 
